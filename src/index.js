5419a8dcdf05d10c273ba3de1200829caba87c2e
Оксана Кухновец <oksana@MacBook-Air-Oksana.local>
2024-08-27 11:06:06 +0200

    change prototype on reverse()

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ module.exports = function towelSort (matrix) {
   if(!matrix){return []}
   return matrix.flatMap((element, index) => {
     if (index % 2 === 1){
-      return element.toReversed()
+      return element.reverse()
     }  else {
       return element
     }
